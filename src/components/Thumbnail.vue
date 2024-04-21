768210cf50c74c22808c0d5ca93eeea20dafee4c
geison-bkz <geison_bf@hotmail.com>
2024-04-20 21:34:10 -0300

    Adicionado artigo 02

--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -6,15 +6,17 @@
                     class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
                 >
                     <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
-                        <h3 class="mb-0">Featured post</h3>
-                        <div class="mb-1 text-body-secondary">Nov 12</div>
+                        <strong class="d-inline-block mb-2 text-primary-emphasis"
+                            >Regulation</strong
+                        >
+                        <h3 class="mb-0">Regulation of the use of AI in the World</h3>
+                        <div class="mb-1 text-body-secondary">Apr 10</div>
                         <p class="card-text mb-auto">
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content.
+                            Nowadays, AI (Artificial Intelligence) has been perfecting itself in a
+                            frightening...
                         </p>
                         <RouterLink
-                            to="/teste"
+                            to="/article-02"
                             class="icon-link gap-1 icon-link-hover stretched-link"
                         >
                             Continue reading
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import Article01 from '@/views/articles/Article-01.vue';
+import Article02 from '@/views/articles/Article-02.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
             path: '/article-01',
             name: 'article-01',
             component: Article01,
+        },
+        {
+            path: '/article-02',
+            name: 'article-02',
+            component: Article02,
         },
         {
             path: '/about',
